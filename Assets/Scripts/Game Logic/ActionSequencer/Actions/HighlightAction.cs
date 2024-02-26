e0e6913d15c75cbd01454f7285bfe426c3161188
doublecubed <34924965+doublecubed@users.noreply.github.com>
2024-02-26 17:00:03 +0300

    HighlightAction added to GameAction system

--- Assets/Scripts/Game Logic/ActionSequencer/Actions/HighlightAction.cs
+++ Assets/Scripts/Game Logic/ActionSequencer/Actions/HighlightAction.cs
@@ -1,8 +1,33 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using DG.Tweening;
 
-public class HighlightAction : MonoBehaviour
+public class HighlightAction : GameAction
 {
+    private Card _card;
+    private Transform _highlightTransform;
+    private float _scaleAmount;
+    private float _highlightDuration;
+    private CancellationToken _cancellationToken;
 
+
+    public HighlightAction(Card card, Transform highlightTransform, float scaleAmount, float highlightDuration, CancellationToken cancellationToken)
+    {
+        _card = card;
+        _highlightTransform = highlightTransform;
+        _scaleAmount = scaleAmount;
+        _highlightDuration = highlightDuration;
+        _cancellationToken = cancellationToken;
+    }
+
+    public override async UniTask ExecuteAction()
+    {
+        float originalScale = _highlightTransform.localScale.x;
+
+        await _highlightTransform.DOScale(originalScale * _scaleAmount, _highlightDuration).SetLoops(1, LoopType.Yoyo).WithCancellation(_cancellationToken);
+
+    }
 }
